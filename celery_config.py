7c7cb811f623d75233dd018b3eb22849bc7f6cbe
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2018-10-10 15:49:19 +0100

    Adding tutorial scripts about celery with redis

---
+++ celery_config.py
@@ -0,0 +1,4 @@
+from celery import Celery
+
+app = Celery('celery_config', broker='redis://localhost:6379/0',
+             include=['pack.celery_fetch'])
---
+++ pack/celery_fetch.py
@@ -0,0 +1,15 @@
+import requests
+
+from celery_config import app
+
+
+@app.task
+def fetch_url(url):
+    resp = requests.get(url)
+    print(resp.status_code)
+
+
+def func(urls):
+    for url in urls:
+        fetch_url.delay(url)
+
